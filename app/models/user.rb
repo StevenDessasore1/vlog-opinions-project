52e15e86181d67e9c8d739bdb69f80c87a2a556b
StevenDessasore <stevendessasoreba@gmail.com>
2021-06-24 03:12:08 -0400

    editing user.rb - validations, uniqueness, password bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+    has_secure_password
+    has_many :opinions, dependent: :destroy #will be deleted if user is deleted
+    validates :name, :email, :password, presence: true #makes sure data is not empty or nil
+    un
 end
