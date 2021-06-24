9bda5994f2a0134fa9b12e88f676591f17df13bc
StevenDessasore <stevendessasoreba@gmail.com>
2021-06-24 04:21:37 -0400

    removed timestamp/added new users.db

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_06_24_064845) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+  end
 
 end
